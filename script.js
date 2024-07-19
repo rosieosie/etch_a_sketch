e67797306094a3d3175ba5e7ee0472f9b78771fb
rosieosie <153328961+rosieosie@users.noreply.github.com>
2024-07-19 09:16:23 +0100

    Fix createGrid function
    Made sure grid is always a specified size and square but number of divs it is composed of can be changed

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ console.log(6)
 function createGrid(gridSize) {
     console.log(gridSize);
     const container = document.querySelector("#container");
+    const squareWidth = 600 / gridSize;
     container.textContent = "";
     for (let i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement("div");
+        div.style.minWidth = `${squareWidth}px`;
+        div.style.height = `${squareWidth}px`;
         div.classList.add("grid-item");
         container.appendChild(div);
     };
 };
 
 
-createGrid(16);
+createGrid(25);
