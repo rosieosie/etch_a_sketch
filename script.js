bc51f7d2068cda5a6959b69e28fb1b901a846ba2
rosieosie <153328961+rosieosie@users.noreply.github.com>
2024-07-24 16:18:01 +0100

    Add function to allow user to adjust grid resolution

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 
-console.log(6)
+createGrid(25);
+
+const resButton = document.querySelector("#res");
+resButton.addEventListener("click", () => {
+    let resNumber 
+    
+    while(true) {
+        resNumber = prompt("Enter a number between 1 & 100");
+
+        if (!isNaN(resNumber) && resNumber >= 1 && resNumber <=100) {
+            break;
+        };
+
+        alert("Please enter a valid number between 1 & 100.");
+    };
+
+    console.log(resNumber);
+    createGrid(resNumber);
+    
+});
 
 function createGrid(gridSize) {     
     console.log(gridSize);
     const container = document.querySelector("#container");
-    const squareWidth = 600 / gridSize;
+    const squareWidth = 800 / gridSize;
     container.textContent = "";
     for (let i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement("div");
@@ -13,6 +32,7 @@ function createGrid(gridSize) {
         div.classList.add("grid-item");
         container.appendChild(div);
     };
+    addMouseDraw();
 };
 
 function addMouseDraw () { 
@@ -21,11 +41,19 @@ function addMouseDraw () {
         console.log("brazil!");
         squares.forEach((square) => {
             square.addEventListener("mouseenter", () => {
-                console.log("egg!");
                 square.classList.add("painted");
             });
         });
 };
 
-createGrid(50);
-addMouseDraw();
+
+
+// TODO 
+// resolution button functionality !
+// start w functioning grid !
+// res button turn to res adjust and clear canvas when canvas painted 
+// separate clear canvas button
+// display canvas resolution
+// change colour + random colour
+// stop and start painting w/ click
+// save file - potentially
